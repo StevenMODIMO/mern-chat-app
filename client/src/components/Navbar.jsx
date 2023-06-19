193c8b588500402f74d3dea063398e173711485a
StevenMODIMO <stevepetes26@gmail.com>
2023-06-19 21:07:01 +0300

    Initial Commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,14 +6,21 @@ import {
   AiOutlineLogin,
   AiFillEdit,
   AiOutlineLogout,
+  AiOutlineUserAdd,
   AiOutlineUser,
 } from "react-icons/ai";
-import { BsChatRightTextFill } from "react-icons/bs"
-import { BiHomeAlt } from "react-icons/bi"
+import {
+  BsChatRightTextFill,
+  BsFacebook,
+  BsTwitter,
+  BsInstagram,
+} from "react-icons/bs";
+import { BiHomeAlt } from "react-icons/bi";
 import { useAuth } from "../context/AuthContext";
-import { motion, AnimatePresence } from "framer-motion"
-import { useMediaQuery } from "@chakra-ui/react"
-import { FaTimes } from "react-icons/fa";
+import { motion, AnimatePresence } from "framer-motion";
+import { useMediaQuery } from "@chakra-ui/react";
+import { FaTimes, FaTiktok } from "react-icons/fa";
+import { MdDeveloperMode } from "react-icons/md";
 
 export default function Navbar() {
   const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
@@ -31,139 +38,119 @@ export default function Navbar() {
   };
   return (
     <>
-       {isLargerThan768 ? (
-          <nav>
-      <div>
-        <NavLink to="/" onClick={closePanel}>
-          <img src={Logo} alt="app-logo" />
-        </NavLink>
-        <div
-          onClick={togglePanel}
+      <nav className="rounded md:flex justify-between md:border-b border-black md:text-green-500 md:bg-black md:rounded-none">
+        <header
+          className={
+            showLinks
+              ? "flex justify-between bg-gray-900 text-green-500 border-b p-2 border-black"
+              : "flex justify-between border-b p-2 border-black md:border-hidden md:bg-gray-900 md:w-2/4 md:pb-4"
+          }
         >
-          {showLinks ? <FaTimes /> : <AiOutlineBars />}
-        </div>
-      </div>
-      <div
-        >
-        {user && (
-          <div>
-            <div>
-            <BsChatRightTextFill className="mt-1" />
-              <NavLink to="/chat" onClick={closePanel}>
-                Chat
-              </NavLink>
-            </div>
-            <div className="flex gap-2 cursor-pointer  rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <AiOutlineUser className="mt-1" />
-              <NavLink to="/profile" onClick={closePanel}>
-                {user.username}
-              </NavLink>
-            </div>
-            <div className="flex gap-2 cursor-pointer  rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <AiOutlineLogout className="mt-1" />
-              <div onClick={logoutEffect}>
-                Logout
+          <div className="flex gap-1 ml-3">
+            <img src={Logo} alt="logo" className="w-6 mt-1 md:mt-1" />
+            <h1 className="text-lg ">mernChatApp</h1>
+          </div>
+          <div onClick={togglePanel} className="text-3xl md:hidden">
+            {showLinks ? <FaTimes /> : <AiOutlineBars />}
+          </div>
+        </header>
+        <main>
+          <nav
+            className={
+              showLinks
+                ? "bg-black  h-screen transition-all duration-300 ease-in-out"
+                : "bg-black  h-0 overflow-hidden transition-all duration-300 ease-in-out md:overflow-visible"
+            }
+          >
+            {!user ? (
+              <div className="text-xl pt-2 text-green-500 md:flex gap-3 md:text-lg">
+                <section
+                  onClick={closePanel}
+                  className="flex gap-1 ml-2 mb-4 bg-gray-900 w-36 p-1 rounded sm:w-48 md:w-fit"
+                >
+                  <BiHomeAlt className="mt-1" />
+                  <NavLink to="/">Home</NavLink>
+                </section>
+                <section
+                  onClick={closePanel}
+                  className="flex gap-1 ml-2 mb-4 bg-gray-900 w-36 p-1 rounded sm:w-48 md:w-fit"
+                >
+                  <AiOutlineUserAdd className="mt-1" />
+                  <NavLink to="/signup">Sign Up</NavLink>
+                </section>
+                <section
+                  onClick={closePanel}
+                  className="flex gap-1 ml-2 mb-4 bg-gray-900 w-36 p-1 rounded sm:w-48 md:w-fit  md:mr-2"
+                >
+                  <AiOutlineLogin className="mt-1" />
+                  <NavLink to="/login">Login</NavLink>
+                </section>
               </div>
-            </div>
-          </div>
-        )}
-
-        {!user && (
-          <div className="h-80 flex flex-col justify-center gap-10 ml-10 md:flex-row md:h-10 md:mt-2">
-          <div whileHover={{ scale: 1.1 }} className="flex gap-2 cursor-pointer rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <BiHomeAlt className="mt-1" />
-              <NavLink to="/" onClick={closePanel}>
-                Home
-              </NavLink>
-            </div>
-            <div className="flex gap-2 cursor-pointer  rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <AiFillEdit className="mt-1" />
-              <NavLink to="/signup" onClick={closePanel}>
-                Signup
-              </NavLink>
-            </div>
-            <div className="flex gap-2 cursor-pointer  rounded mr-12 p-2 md:p-2 hover:bg-zinc-800">
-              <AiOutlineLogin className="mt-1" />
-              <NavLink to="/login" onClick={closePanel}>
-                Login
-              </NavLink>
-            </div>
-          </div>
-        )}
-      </div>
-    </nav>
-       ) : (
-          <nav className="md:flex justify-between">
-      <div className="flex justify-between md:mt-2">
-        <NavLink to="/" onClick={closePanel}>
-          <img src={Logo} alt="app-logo" className="w-48 m-2 md:mt-2" />
-        </NavLink>
-        <div
-          className="text-3xl  m-2 md:hidden"
-          onClick={togglePanel}
-        >
-          {showLinks ? <FaTimes /> : <AiOutlineBars />}
-        </div>
-      </div>
-      <AnimatePresence>
-      <motion.div className={
-          showLinks
-            ? "absolute h-80 w-72 m-4 bg-zinc-900/95 rounded  md:relative flex-row text-3xl"
-            : "hidden md:flex flex-row text-2xl"
-        } 
-          initial={{ scale: 0 }}
-          whileInView={{ scale: 1 }}
-          transition={{ type: "spring", stiffness: 200, duration: 1 }}
-        >
-        {user && (
-          <div className="h-80 flex flex-col justify-center gap-10 ml-10 md:flex-row md:h-10 md:mt-2">
-            <motion.div whileHover={{ scale: 1.1 }} className="flex gap-2 cursor-pointer shadow-sm shadow-zinc-900 rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <BsChatRightTextFill className="mt-1" />
-              <NavLink to="/chat" onClick={closePanel}>
-                Chat
-              </NavLink>
-            </motion.div>
-            <motion.div whileHover={{ scale: 1.1 }} className="flex gap-2 cursor-pointer shadow-sm shadow-zinc-900 rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <AiOutlineUser className="mt-1" />
-              <NavLink to="/profile" onClick={closePanel}>
-                {user.username}
-              </NavLink>
-            </motion.div>
-            <motion.div whileHover={{ scale: 1.1 }} className="flex gap-2 cursor-pointer shadow-sm shadow-zinc-900 rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <AiOutlineLogout className="mt-1" />
-              <div onClick={logoutEffect}>
-                Logout
+            ) : (
+              <div className="text-xl pt-2 text-green-500 md:flex gap-3 md:text-lg">
+                <section
+                  onClick={closePanel}
+                  className="flex gap-1 ml-2 mb-4 bg-gray-900 w-36 p-1 rounded sm:w-48 md:w-fit bg-gray-900"
+                >
+                  <BsChatRightTextFill className="mt-1" />
+                  <NavLink to="/chat">Chat</NavLink>
+                </section>
+                <section
+                  onClick={closePanel}
+                  className="flex gap-1 ml-2 mb-4 bg-gray-900 w-36 p-1 rounded sm:w-48 md:w-fit"
+                >
+                  <AiOutlineUser className="mt-1" />
+                  <NavLink to="/profile">Profile</NavLink>
+                </section>
+                <section
+                  onClick={logoutEffect}
+                  className="flex gap-1 ml-2 mb-4 bg-gray-900 w-36 p-1 rounded sm:w-48 md:w-fit md:mr-2 cursor-pointer"
+                >
+                  <AiOutlineLogout className="mt-1" />
+                  <h1>Logout</h1>
+                </section>
               </div>
-            </motion.div>
-          </div>
-        )}
-
-        {!user && (
-          <div className="h-80 flex flex-col justify-center gap-10 ml-10 md:flex-row md:h-10 md:mt-2">
-           <motion.div whileHover={{ scale: 1.1 }} className="flex gap-2 cursor-pointer shadow-sm shadow-zinc-900 rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <BiHomeAlt className="mt-1" />
-              <NavLink to="/" onClick={closePanel}>
-                Home
-              </NavLink>
-            </motion.div>
-            <motion.div whileHover={{ scale: 1.1 }} className="flex gap-2 cursor-pointer shadow-sm shadow-zinc-900 rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <AiFillEdit className="mt-1" />
-              <NavLink to="/signup" onClick={closePanel}>
-                Signup
-              </NavLink>
-            </motion.div>
-            <motion.div whileHover={{ scale: 1.1 }} className="flex gap-2 cursor-pointer shadow-sm shadow-zinc-900 rounded mr-12 p-2 md:p-2 shadow-none  hover:bg-zinc-800">
-              <AiOutlineLogin className="mt-1" />
-              <NavLink to="/login" onClick={closePanel}>
-                Login
-              </NavLink>
-            </motion.div>
-          </div>
-        )}
-      </motion.div>
-      </AnimatePresence>
-    </nav>
-       )}
+            )}
+            <main className="text-green-500  mb-5 md:hidden">
+              <header className="ml-2 border-b-2 w-fit border-green-500 sm:w-44">
+                <h1>Upcoming Features</h1>
+              </header>
+              <div className="text-xl ml-8 mt-2">
+                <section className="flex gap-1">
+                  <MdDeveloperMode className="mt-1" />
+                  <h1>Developers API</h1>
+                </section>
+              </div>
+            </main>
+            <main className="text-green-500 md:hidden">
+              <header className="ml-2 border-b-2 w-fit border-green-500 sm:w-44">
+                <h1>Follow Us</h1>
+              </header>
+              <div className="">
+                <section className="flex gap-1 text-xl mt-2 ml-6 mb-4">
+                  <BsFacebook className="mt-1" />
+                  <h1>facebook</h1>
+                </section>
+                <section className="flex gap-1 text-xl ml-6 mb-4">
+                  <BsTwitter className="mt-1" />
+                  <h1>twitter</h1>
+                </section>
+                <section className="flex gap-1 text-xl ml-6 mb-4">
+                  <BsInstagram className="mt-1" />
+                  <h1>instagram</h1>
+                </section>
+                <section className="flex gap-1 text-xl ml-6 mb-4">
+                  <FaTiktok className="mt-1 text-2xl" />
+                  <h1>tiktok</h1>
+                </section>
+              </div>
+            </main>
+            <footer className="text-green-400 rounded w-fit mx-auto p-1 bg-gray-900 text-center md:hidden">
+              <span className="text-xs">&copy; mernChatApp 2023</span>
+            </footer>
+          </nav>
+        </main>
+      </nav>
     </>
   );
 }
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,88 +1,42 @@
-import { RiGroup2Fill } from "react-icons/ri"
-import { BsFillChatLeftTextFill } from "react-icons/bs"
-import { MdOutlinePrivacyTip } from "react-icons/md"
-import { GiJoint } from "react-icons/gi"
-import { FaShare } from "react-icons/fa"
-import { motion } from "framer-motion"
-import { CgProfile } from "react-icons/cg"
-import { IoIosCreate } from "react-icons/io"
-import { BsFillChatRightDotsFill, BsShareFill } from "react-icons/bs"
+import { RiGroup2Fill } from "react-icons/ri";
+import { BsFillChatLeftTextFill } from "react-icons/bs";
+import { MdOutlinePrivacyTip } from "react-icons/md";
+import { GiJoint } from "react-icons/gi";
+import { FaShare } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { CgProfile } from "react-icons/cg";
+import { IoIosCreate } from "react-icons/io";
+import { BsFillChatRightDotsFill, BsShareFill } from "react-icons/bs";
 
 export default function Landing() {
   return (
     <div className="h-fit flex flex-col mt-10 md:items-center lg:mt-10">
-     <section>
-         <header className="text-center">
-        <div className="text-5xl ">Hi!</div>
-        <div className="text-2xl">Welcome to CoNvErSaTe</div>
-      </header>
-      <main>
-        <motion.div initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring" ,stiffness: 300}} className="flex justify-center text-9xl">
-          <RiGroup2Fill />
-        </motion.div>
-        <motion.div initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring" ,stiffness: 300}} >
-           <h1 className="font-mono text-center text-lg md:w-3/6 mx-auto">CoNvErSaTe is a room based messaging platform that enables people to connect and exchange on various aspects of our daily lives</h1>
-        </motion.div>
-      </main>
-      <main className="mt-20 font-mono md:flex flex-col items-center lg:mt-40">
-         <h1 className="text-5xl text-center">Features</h1>
-         <motion.div initial={{ opacity: 0, scale: 0.9 }} whileInView={{ opacity: 1, scale: 1}} transition={{ type: "spring", stiffness: 200}} className="flex gap-3 m-1 mt-5">
-           <div className="text-5xl">
-           <BsFillChatLeftTextFill />
-         </div>
-         <div className="text-2xl mt-2">Instant Messaging</div>
-         </motion.div>
-         <motion.div initial={{ opacity: 0, scale: 0.9 }} whileInView={{ opacity: 1, scale: 1}} transition={{ type: "spring", stiffness: 200}} className="flex gap-2 m-1 mt-5">
-            <div className="text-5xl">
-              <FaShare />
-            </div>
-            <div className="text-2xl mt-2">Share Your Rooms</div>
-         </motion.div>
-         <motion.div initial={{ opacity: 0, scale: 0.9 }} whileInView={{ opacity: 1, scale: 1}} transition={{ type: "spring", stiffness: 200}} className="flex gap-2 m-1 mt-5">
-            <div className="text-5xl">
-              <GiJoint />
-            </div>
-            <div className="text-2xl mt-2">Join Other Rooms</div>
-         </motion.div>
-         <motion.div initial={{ opacity: 0, scale: 0.9 }} whileInView={{ opacity: 1, scale: 1}} transition={{ type: "spring", stiffness: 200, duration: 4}} className="flex gap-2 m-1 mt-5">
-            <div className="text-5xl">
-              <MdOutlinePrivacyTip />
-            </div>
-            <div className="text-2xl mt-2">Protected Privacy</div>
-         </motion.div>
-      </main>
-      <main className="mt-20 font-mono">
-         <h1 className="text-5xl text-center">Get Started</h1>
-        <section className="md: flex flex-col items-center">
-           <div className=" flex flex-col items-center gap-2 shadow shadow-black m-3 rounded p-1 md:w-80">
-            <div className="text-7xl">
-              <CgProfile />
-            </div>
-            <div className="text-2xl">Set Up Your Profile</div>
-         </div>
-         <div className=" flex flex-col items-center gap-2 shadow shadow-black m-3 rounded p-1 md:w-80">
-            <div className="text-7xl">
-              <IoIosCreate />
-            </div>
-            <div className="text-2xl">Create Your Room</div>
-         </div>
-         <div className=" flex flex-col items-center gap-2 shadow shadow-black m-3 rounded p-1 md:w-80">
-            <div className="text-7xl">
-              <BsFillChatRightDotsFill />
-            </div>
-            <div className="text-2xl">Start Chatting</div>
-         </div>
-         <div className=" flex flex-col items-center gap-2 shadow shadow-black m-3 rounded p-1 md:w-80">
-            <div className="text-7xl">
-              <BsShareFill />
-            </div>
-            <div className="text-2xl">Connect With Friends</div>
-         </div>
-        </section>
-      </main>
-     </section>
-     
-     <footer>&copy;All rights reserved ==== Convertsate 2023</footer>
+      <section>
+        <header className="text-center">
+          <div className="text-5xl ">Hi!</div>
+          <div className="text-2xl">Welcome to mernChatApp</div>
+        </header>
+        <main>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "spring", stiffness: 300 }}
+            className="flex justify-center text-9xl"
+          >
+            <RiGroup2Fill />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "spring", stiffness: 300 }}
+          >
+            <h1 className="font-mono text-center text-lg md:w-3/6 mx-auto">
+              mernChatApp is a room based messaging platform that enables people
+              to connect and exchange on various aspects of their daily lives
+            </h1>
+          </motion.div>
+        </main>
+      </section>
     </div>
   );
 }
