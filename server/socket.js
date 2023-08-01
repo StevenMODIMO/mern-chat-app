719fe90cd263fd12c04c826aad1f0e6944ff7a2f
StevenMODIMO <stevepetes26@gmail.com>
2023-08-01 05:54:51 +0300

    Initial Commit

--- server/socket.js
+++ server/socket.js
@@ -1,30 +1,40 @@
-const { Server } = require("socket.io")
-const Chat = require("./models/chatModel")
+const { Server } = require("socket.io");
+const Chat = require("./models/chatModel");
 
-module.exports = function(server) {
-const io = new Server(server, {
+module.exports = function (server) {
+  const io = new Server(server, {
     cors: {
-        origin: "https://brilliant-cuchufli-cc2e4b.netlify.app",
-        methods: ["GET", "POST"]
-    }
-})
+      origin: "https://chat-app-3fog.onrender.com/",
+      methods: ["GET", "POST"],
+    },
+  });
   io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+    console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join-room", (data) => {
-    socket.join(data);
-    const room = io.sockets.adapter.rooms
-    socket.emit("room-joined", {room: data})
-  });
+    socket.on("join-room", (data) => {
+      socket.join(data);
+      const room = io.sockets.adapter.rooms;
+      socket.emit("room-joined", { room: data });
+    });
 
-  socket.on("message", async (data) => {
-  io.to(data.room).emit("received", data);
-      await Chat.findOneAndUpdate({roomName: data.room}, {$push: { chats: { sender: data.sender, message: data.message, time: data.time}}})
-      
-  });
+    socket.on("message", async (data) => {
+      io.to(data.room).emit("received", data);
+      await Chat.findOneAndUpdate(
+        { roomName: data.room },
+        {
+          $push: {
+            chats: {
+              sender: data.sender,
+              message: data.message,
+              time: data.time,
+            },
+          },
+        }
+      );
+    });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
   });
-});
-}
+};
